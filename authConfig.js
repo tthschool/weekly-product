ad6fa9a83038695a20ffc4d557ba9092dfc18fd1
hoang <tth251294@gmail.com>
2025-07-16 15:35:46 +0900

    role added

--- authConfig.js
+++ authConfig.js
@@ -65,7 +65,7 @@ export const graphConfig = {
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 export const loginRequest = {
-    scopes: ["User.Read"]
+    scopes: ["User.Read","Mail.Read"]
 };
 
 // exporting config object for jest
